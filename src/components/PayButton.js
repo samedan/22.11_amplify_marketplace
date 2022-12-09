d93697a8912f68bc30409c87e987c9684dc8a546
Daniel Popescu <same.dan@gmail.com>
2022-12-09 09:34:54 +0100

    Stripe Works

--- src/components/PayButton.js
+++ src/components/PayButton.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Notification, Message } from "element-react";
 import StripeCheckout from "react-stripe-checkout";
+import { API } from "aws-amplify";
 
 const stripeConfig = {
   currency: "USD",
@@ -8,8 +9,31 @@ const stripeConfig = {
 };
 
 const PayButton = ({ product, user }) => {
+  const handleCharge = async (token) => {
+    try {
+      const result = await API.post(
+        "orderlambda", // name of the LambdaFunction
+        "/charge", // path
+        {
+          body: {
+            token,
+            charge: {
+              currency: stripeConfig.currency,
+              amount: product.price,
+              description: product.description,
+            },
+          },
+        }
+      );
+      console.log({ result });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <StripeCheckout
+      token={handleCharge}
       currency={stripeConfig.currency}
       stripeKey={stripeConfig.publishableAPIKey}
       email={user.attributes.email}
